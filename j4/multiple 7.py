9809a73b139bc6888ec5dc2f3d8711ff7dfd5389
navidreza hadian <92804948+navidreza-hadian@users.noreply.github.com>
2023-08-06 02:25:41 +0330

    Add files via upload

---
+++ j4/multiple 7.py
@@ -0,0 +1,28 @@
+a = int(input('enter number :'))
+
+if a % 7 == 0 :
+    print('yes multiple 7')
+else :
+    a += 1
+    if a % 7 == 0 :
+        print('no 7 but :',a)
+    else :
+        a += 1
+        if a % 7 == 0 :
+            print('no 7 but :',a)
+        else :
+            a += 1
+            if a % 7 == 0 :
+                print('no 7 but :',a)
+            else :
+                a += 1 
+                if a % 7 == 0 :
+                    print('no 7 but :',a)
+                else:
+                    a += 1 
+                    if a % 7 == 0 :
+                        print('no 7 but :',a)
+                    else:
+                        a += 1
+                        if a % 7 == 0 :
+                            print('no 7 but :',a)
