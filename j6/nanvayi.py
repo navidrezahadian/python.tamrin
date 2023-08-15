e6dc21b9e243145bf6d2cbaad1482c60ba0b70c1
navidreza hadian <92804948+navidrezahadian@users.noreply.github.com>
2023-08-15 16:55:01 +0330

    Add files via upload

---
+++ j6/nanvayi.py
@@ -0,0 +1,18 @@
+n = ['Ahmad','Milad','Ashkan','Mohammad','Saeed','Dariush']
+
+print('saf =',n)
+
+a = str(input('enter name : '))
+aa = int(input('enter place : '))
+
+b = str(input('enter name : '))
+bb = int(input('enter place : '))
+
+c = str(input('enter name : '))
+cc = int(input('enter place : '))
+
+n.insert(aa,a)
+n.insert(bb,b)
+n.insert(cc,c)
+
+print(n)
