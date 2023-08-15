83855920e21e0b355a03cb3cfb51a909ee29677d
navidreza hadian <92804948+navidrezahadian@users.noreply.github.com>
2023-08-15 16:35:50 +0330

    Add files via upload

---
+++ j5/list.py
@@ -0,0 +1,16 @@
+a = ['n',23,5,8,[8,77,'t'],45,'py']
+z = ['hh','nn']
+
+print('a =', a)
+print('z =', z)
+
+print (a[3])
+print (a[4][1])
+print (a[-1])
+print (a * 2)
+print (a[:2])
+print (a[:5:2])
+print (a + z)
+print (a[::-2])
+print (a[5:8:3])
+print (a + [555])
