a17a7dabc56165a3afbb88e2dcd51f37133c0aa9
navidreza hadian <92804948+navidrezahadian@users.noreply.github.com>
2023-08-15 17:12:42 +0330

    Add files via upload

---
+++ j7/list in list.py
@@ -0,0 +1,16 @@
+a = ["py","i"," ","popul","lang"]
+b = ["thon","s","a","ar","uage."]
+c = []
+
+print('a =',a)
+print('b =',b)
+
+c.append(a[0] + b[0])
+c.append(a[1] + b[1])
+c.append(a[2] + b[2])
+c.append(a[3] + b[3])
+c.append(a[4] + b[4])
+
+print('list 1 :', a)
+print('list 2 :', b)
+print('chasbandan yek be yek 1,2 :', c)
