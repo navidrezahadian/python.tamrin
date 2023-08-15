b3e477ee57223b08ae0dabe5db3866b4ed310919
navidreza hadian <92804948+navidrezahadian@users.noreply.github.com>
2023-08-15 16:57:52 +0330

    Add files via upload

---
+++ j6/sara.py
@@ -0,0 +1,7 @@
+x = ['Homa','sara','Amir mahdi','Pooria','sara','Shahriyar','Atefeh','sara','Ali','Sina','sara','sara','Navid reza','Sadra']
+b = ' '.join(x)
+b = b.split('sara')
+
+print('x =', x)
+
+print('x > sara =', b)
