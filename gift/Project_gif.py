41ffb1f1a22461e42ab9f603fa6214fcbf3b603b
navidreza hadian <92804948+navidrezahadian@users.noreply.github.com>
2023-08-25 16:31:48 +0330

    Add files via upload

---
+++ gift/Project_gif.py
@@ -0,0 +1,10 @@
+import imageio.v2 as imag2
+import os
+
+images = []
+
+for file_name in os.listdir('emtehan\image'):
+    img = imag2.imread('emtehan\image' + '/' + file_name)
+    images.append(img)
+
+imag2.mimsave('emtehan\gift\Flower.gif', images)
