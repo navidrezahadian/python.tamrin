b06ba1e63c534ca766b623255b7b5ee76174d0c2
navidreza hadian <92804948+navidrezahadian@users.noreply.github.com>
2023-08-27 12:03:53 +0330

    Add files via upload

---
+++ j7/sum number.py
@@ -0,0 +1,33 @@
+a = input('enter your sentence = ')
+
+numbers = 0
+
+for letter in a:
+    if letter == '1':
+        numbers += int(letter)
+        continue
+    elif letter == '2':
+        numbers += int(letter)
+        continue
+    elif letter == '3':
+        numbers += int(letter)
+        continue
+    elif letter == '4':
+        numbers += int(letter)
+        continue
+    elif letter == '5':
+        numbers += int(letter)
+        continue
+    elif letter == '6':
+        numbers += int(letter)
+        continue
+    elif letter == '7':
+        numbers += int(letter)
+        continue
+    elif letter == '8':
+        numbers += int(letter)
+        continue
+    elif letter == '9':
+        numbers += int(letter)
+        continue
+print(numbers)
